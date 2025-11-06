34a2183cad936d527d9d497fa404d2bc74d3cf89
sporaki13891 <sporaki13891@gmail.com>
2025-11-06 22:23:06 +0200

    force vercel rebuild

--- app/api/get/route.ts
+++ app/api/get/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
+// force vercel rebuild
+
 export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
